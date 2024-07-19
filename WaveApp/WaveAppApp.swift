7485af5f94b80942a2ad95f36c243e6e4573f982
Diego Hsue <dehsu@usc.edu>
2024-07-18 17:17:11 -0700

    Initial Commit

---
+++ WaveApp/WaveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveAppApp.swift
+//  WaveApp
+//
+//  Created by Diego Hsu on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WaveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaveAppUITests/WaveAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WaveAppUITestsLaunchTests.swift
+//  WaveAppUITests
+//
+//  Created by Diego Hsu on 7/18/24.
+//
+
+import XCTest
+
+final class WaveAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
